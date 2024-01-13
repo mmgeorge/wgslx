e957374904caa069f1c9a34369c0af6b3e287d5d
Matt George <mattmg412@gmail.com>
2024-01-13 14:50:24 -0800

    Add nested test case

--- tests/tests/wgslx.rs
+++ tests/tests/wgslx.rs
@@ -4,3 +4,8 @@ use internal::{compile_module};
 fn import_twice () {
   compile_module("tests/import-twice/lib.wgslx", false).unwrap(); 
 }
+
+#[test]
+fn import_twice_relative () {
+  compile_module("tests/import-twice-relative/lib.wgslx", false).unwrap(); 
+}

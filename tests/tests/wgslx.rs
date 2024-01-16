6440fa59edf5ef1efbd3ff56cc6c6de6062db342
Matt George <mattmg412@gmail.com>
2024-01-15 19:40:25 -0800

    Add test, move to module declaration

--- tests/tests/wgslx.rs
+++ tests/tests/wgslx.rs
@@ -2,10 +2,10 @@ use internal::{compile_module};
 
 #[test]
 fn import_twice () {
-  compile_module("tests/import-twice/lib.wgslx", false).unwrap(); 
+  compile_module("tests/import-twice", false).unwrap(); 
 }
 
 #[test]
 fn import_twice_relative () {
-  compile_module("tests/import-twice-relative/lib.wgslx", false).unwrap(); 
+  compile_module("tests/import-twice-relative", false).unwrap(); 
 }
